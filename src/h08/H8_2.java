8255887dec6d993f81105eef61eb5e2d3fbaddcc
Ovab <bavo.famknol@gmail.com>
2020-09-14 11:56:14 +0200

    8.2 af

--- src/h08/H8_2.java
+++ src/h08/H8_2.java
@@ -12,11 +12,19 @@ public class H8_2 extends Applet {
     Button vrouw;
     Button manlln;
     Button vrouwlln;
+    Button totaal;
 
     TextField manT;
     TextField vrouwT;
     TextField manllnT;
     TextField vrouwllnT;
+    TextField totaalT;
+
+    int methM;
+    int methV;
+    int methML;
+    int methVL;
+    int totaalM;
 
 
     public void init() {
@@ -24,57 +32,88 @@ public class H8_2 extends Applet {
         vrouwT = new TextField("", 30);
         manllnT = new TextField("", 30);
         vrouwllnT = new TextField("", 30);
+        totaalT = new TextField("", 20);
+
         man = new Button("man");
         vrouw = new Button("vrouw");
         manlln = new Button("Joch");
         vrouwlln = new Button("Vrouwlijke leerling");
+        totaal = new Button("totaal");
 
         man.addActionListener( new H8_2.KnopMListener() );
         vrouw.addActionListener( new H8_2.KnopVListener() );
         manlln.addActionListener( new H8_2.KnopMLListener() );
         vrouwlln.addActionListener( new H8_2.KnopVLListener() );
+        totaal.addActionListener( new H8_2.KnoptotListener() );
 
         add (man);
         add (vrouw);
         add (manlln);
         add (vrouwlln);
+        add (totaal);
 
         add (manT);
         add (vrouwT);
         add (manllnT);
         add (vrouwllnT);
+
+        add (totaalT);
+
+        methM = 0;
+        methV = 0;
+        methML = 0;
+        methVL = 0;
+        totaalM = methM + methV + methML + methVL;
     }
 
 
 
 
     public void paint(Graphics g) {
-
-
     }
+
     class KnopMListener implements ActionListener	{
         public void actionPerformed( ActionEvent e ) {
-            manT.setText("HAHA GEt SCHERKT");
+            methM++;
+            totaalM++;
+            manT.setText(String.valueOf(methM));
             repaint();
         }
     }
+
     class KnopVListener implements ActionListener	{
         public void actionPerformed( ActionEvent e ) {
-            vrouwT.setText("HAHA GEt SCHERKT");
+            methV++;
+            totaalM++;
+            vrouwT.setText(String.valueOf(methV));
             repaint();
         }
     }
+
     class KnopMLListener implements ActionListener	{
         public void actionPerformed( ActionEvent e ) {
-            manllnT.setText("HAHA GEt SCHERKT");
+            methML++;
+            totaalM++;
+            manllnT.setText(String.valueOf(methML));
             repaint();
         }
     }
+
     class KnopVLListener implements ActionListener	{
         public void actionPerformed( ActionEvent e ) {
-            vrouwllnT.setText("HAHA GEt SCHERKT");
+            methVL++;
+            totaalM++;
+            vrouwllnT.setText(String.valueOf(methVL));
             repaint();
         }
     }
+
+    class KnoptotListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            totaalT.setText(String.valueOf(totaalM));
+            repaint();
+        }
+    }
+
 
 }
