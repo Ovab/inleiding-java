a617b6e6d206b5fc5bdbcedd4c202644835ad486
Ovab <bavo.famknol@gmail.com>
2020-09-14 12:35:35 +0200

    hey

---
+++ src/h08/H8_3.java
@@ -0,0 +1,24 @@
+package h08;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class H8_3 extends Applet {
+    Button ok;
+    TextField hey;
+
+    public void init() {
+    ok = new Button("OK");
+
+
+    }
+
+
+
+
+    public void paint(Graphics g) {
+
+
+
+    }
+}
