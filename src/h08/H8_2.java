a5992e28e155e8a30d20866ad378f92df13b43e2
Ovab <bavo.famknol@gmail.com>
2020-09-14 10:50:30 +0200

    bliep

--- src/h08/H8_2.java
+++ src/h08/H8_2.java
@@ -1,5 +1,6 @@
 package h08;
 
+import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
@@ -12,12 +13,36 @@ public class H8_2 extends Applet {
     Button manlln;
     Button vrouwlln;
 
+    TextField manT;
+    TextField vrouwT;
+    TextField manllnT;
+    TextField vrouwllnT;
+
 
     public void init() {
-        man = new Button("man");        vrouw = new Button("vrouw");        manlln = new Button("Joch");        vrouwlln = new Button("Vrouwlijke leerling");
+        manT = new TextField("", 30);
+        vrouwT = new TextField("", 30);
+        manllnT = new TextField("", 30);
+        vrouwllnT = new TextField("", 30);
+        man = new Button("man");
+        vrouw = new Button("vrouw");
+        manlln = new Button("Joch");
+        vrouwlln = new Button("Vrouwlijke leerling");
 
+        man.addActionListener( new H8_2.KnopMListener() );
+        vrouw.addActionListener( new H8_2.KnopVListener() );
+        manlln.addActionListener( new H8_2.KnopMLListener() );
+        vrouwlln.addActionListener( new H8_2.KnopVLListener() );
 
+        add (man);
+        add (vrouw);
+        add (manlln);
+        add (vrouwlln);
 
+        add (manT);
+        add (vrouwT);
+        add (manllnT);
+        add (vrouwllnT);
     }
 
 
@@ -26,6 +51,30 @@ public class H8_2 extends Applet {
     public void paint(Graphics g) {
 
 
-
     }
+    class KnopMListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            manT.setText("HAHA GEt SCHERKT");
+            repaint();
+        }
+    }
+    class KnopVListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            vrouwT.setText("HAHA GEt SCHERKT");
+            repaint();
+        }
+    }
+    class KnopMLListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            manllnT.setText("HAHA GEt SCHERKT");
+            repaint();
+        }
+    }
+    class KnopVLListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            vrouwllnT.setText("HAHA GEt SCHERKT");
+            repaint();
+        }
+    }
+
 }
