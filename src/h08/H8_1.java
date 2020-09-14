ade2fb1edbe13b4914f7253fa4ba0f046c61d49f
Ovab <bavo.famknol@gmail.com>
2020-09-14 09:53:58 +0200

    wat dingetjes gedaan

--- src/h08/H8_1.java
+++ src/h08/H8_1.java
@@ -1,18 +1,39 @@
 package h08;
 
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
 
 public class H8_1 extends Applet {
-
-
+    TextField tekstvak;
+    Button knop;
+    Button boop;
 
     public void init() {
+        tekstvak = new TextField("", 30);
+        knop = new Button("Ok");
+        boop = new Button("reset");
+        knop.addActionListener( new KnopListener() );
+        boop.addActionListener( new BoopListener() );
+        add(tekstvak);
+        add(knop);
+        add(boop);
     }
 
-
-
-
     public void paint(Graphics g) {
+        g.drawString("It's not like I want you to type here BAKA!", 50, 60 );
+    }
+
+    class KnopListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            tekstvak.setText("HAHA GEt SCHERKT");
+            repaint();
+        }
+    }
+    class BoopListener implements ActionListener    {
+        public void actionPerformed(ActionEvent e) {
+            tekstvak.setText("");
+            repaint();
+        }
     }
 }
---
+++ src/h08/H8_2.java
@@ -0,0 +1,31 @@
+package h08;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class H8_2 extends Applet {
+    //belangrijke shit I guess, het is maar initializen
+    Button man;
+    Button vrouw;
+    Button manlln;
+    Button vrouwlln;
+
+
+    public void init() {
+        man = new Button("man");        vrouw = new Button("vrouw");        manlln = new Button("Joch");        vrouwlln = new Button("Vrouwlijke leerling");
+
+
+
+    }
+
+
+
+
+    public void paint(Graphics g) {
+
+
+
+    }
+}
