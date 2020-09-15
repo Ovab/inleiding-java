b56a747c10dc8bd793493b5b2a164857cea1ab28
Ovab <bavo.famknol@gmail.com>
2020-09-15 08:22:48 +0200

    H8.3 done

--- src/h08/H8_3.java
+++ src/h08/H8_3.java
@@ -1,24 +1,38 @@
 package h08;
 
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
 
 public class H8_3 extends Applet {
+    TextField tekstvak;
     Button ok;
-    TextField hey;
+    Label label;
+    double getal;
 
     public void init() {
-    ok = new Button("OK");
-
-
+        tekstvak = new TextField("", 20);
+        ok = new Button ("ok");
+        ok.addActionListener( new TekstvakListener() );
+        label = new Label("Type een getal");
+        tekstvak.addActionListener( new TekstvakListener() );
+        add (ok);
+        add(label);
+        add(tekstvak);
     }
 
-
-
-
     public void paint(Graphics g) {
+        g.drawString("De prijs incl. btw is " + getal + " Euro",50, 60 );
+    }
 
-
-
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak.getText();
+            getal = Double.parseDouble( s );
+            getal = getal*1.21;
+            //Je kan deze regels ook samenvoegen
+            //getal = Double.parseDouble(tekstvak.getText());
+            repaint();
+        }
     }
 }
