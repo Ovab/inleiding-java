3e8cd73667025a65ea51c391d435441d3b5c583b
Ovab <bavo.famknol@gmail.com>
2020-09-17 12:50:25 +0200

    laat maar

--- src/h11/H11_2.java
+++ src/h11/H11_2.java
@@ -7,6 +7,7 @@ import java.applet.*;
 public class H11_2 extends Applet {
     double teller;
     int y = 9;
+    int x = 0;
 
     public void init() {
 
@@ -16,6 +17,7 @@ public class H11_2 extends Applet {
     public void paint(Graphics g) {
         for(teller = 0; teller <11; teller++) {
             y += 1;
+            x += 20;
             g.drawString("" +y, 50, x);
         }
     }
