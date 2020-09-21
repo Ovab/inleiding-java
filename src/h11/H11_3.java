a3f3a59e0f68761dc65d4a82daba2e34e8e01193
Ovab <bavo.famknol@gmail.com>
2020-09-21 13:50:29 +0200

    h11 AF

--- src/h11/H11_3.java
+++ src/h11/H11_3.java
@@ -5,17 +5,24 @@ import java.applet.*;
 
 
 public class H11_3 extends Applet {
-    double teller;
-    int y = 9;
-    int x = 0;
+    int teller;
+    int a;
+    int b;
+    int c;
+    int y;
 
     public void init() {
-
+        a = 0;
+        b = 1;
     }
 
     public void paint(Graphics g) {
-        for(teller = 0; teller <11; teller++) {
-
+        for (teller = 0; teller < 11; teller++) {
+            g.drawString(" " + c, 50, y);
+            a = b;
+            b=c;
+            c = a + b;
+            y += 20;
         }
     }
 }
--- src/h11/Praktijk.java
+++ src/h11/Praktijk.java
@@ -13,7 +13,7 @@ public class Praktijk extends Applet {
     int getal2;
     int uit, uit1, uit2, uit3, uit4, uit5, uit6, uit7, uit8, uit9;
     int y = 0;
-    double teller;
+    double teller, toet;
 
     public void init() {
         inputvak = new TextField("", 20);
@@ -30,15 +30,15 @@ public class Praktijk extends Applet {
             getal2 += 1;
             y += 20;
             g.drawString(getal1 + "x1=" + uit, 50, 10);
-            g.drawString(getal1 + "x2=" + uit, 50, 30);
-            g.drawString(getal1 + "x3=" + uit, 50, 50);
-            g.drawString(getal1 + "x4=" + uit, 50, 70);
-            g.drawString(getal1 + "x5=" + uit, 50, 90);
-            g.drawString(getal1 + "x6=" + uit, 50, 110);
-            g.drawString(getal1 + "x7=" + uit, 50, 130);
-            g.drawString(getal1 + "x8=" + uit, 50, 150);
-            g.drawString(getal1 + "x9=" + uit, 50, 170);
-            g.drawString(getal1 + "x10=" + uit, 50, 190);
+            g.drawString(getal1 + "x2=" + uit1, 50, 30);
+            g.drawString(getal1 + "x3=" + uit2, 50, 50);
+            g.drawString(getal1 + "x4=" + uit3, 50, 70);
+            g.drawString(getal1 + "x5=" + uit4, 50, 90);
+            g.drawString(getal1 + "x6=" + uit5, 50, 110);
+            g.drawString(getal1 + "x7=" + uit6, 50, 130);
+            g.drawString(getal1 + "x8=" + uit7, 50, 150);
+            g.drawString(getal1 + "x9=" + uit8, 50, 170);
+            g.drawString(getal1 + "x10=" + uit9, 50, 190);
         }
     }
 
@@ -49,7 +49,7 @@ public class Praktijk extends Applet {
             getal1 = Integer.parseInt(s);
             y = 0;
 
-            for (teller = 0; teller < 10; teller++) {
+            for (toet = 0; toet < 10; toet++) {
                 uit = getal1 * 1;
                 uit1 = getal1 * 2;
                 uit2 = getal1 * 3;
