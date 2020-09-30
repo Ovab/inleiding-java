2fa7dc5c15085c3b2129790543fe9e2b9d3b1876
Ovab <bavo.famknol@gmail.com>
2020-09-30 12:26:19 +0200

    still working on 12.4

--- src/H12/H12_4.java
+++ src/H12/H12_4.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class H12_4 extends Applet {
-    int[] searching = {69, 69420, 420};
+    int[] searching;
     int gezocht;
     TextField txtfield;
     Button okeknop;
@@ -15,8 +15,10 @@ public class H12_4 extends Applet {
     boolean jesno = false;
 
 
+
     public void init() {
-        searching = new int[2];
+
+        searching = new int[3];
         txtfield = new TextField("", 10);
         add(txtfield);
 
@@ -27,10 +29,10 @@ public class H12_4 extends Applet {
 
 
     public void paint(Graphics g) {
-        if (jesno = true) {
+        if (jesno == true) {
             g.drawString("de waarde is gevonden " + out, 50 ,100);
         }
-        if (jesno = false) {
+        if (jesno == false) {
             g.drawString("ERROR: FUNNY NUMBER NOT FOUND", 50 ,120);
         }
     }
@@ -38,17 +40,29 @@ public class H12_4 extends Applet {
     //classes
     class InputListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+
+            searching [0] = 69;
+            searching [1] = 69420;
+            searching [2] = 420;
+
+            //getting txt
             String b = txtfield.getText();
             gezocht = Integer.parseInt(b);
-
+            //debug stuff
+            System.out.println("input is "+ gezocht);
+            //is the number funny
             for (int i = 0; i < searching.length; i++) {
+                System.out.println("i is bij "+out);
+
+                System.out.println(searching[i]);
                 if (gezocht == searching [i]) {
+                    out = i;
                     jesno = true;
-                    break;
                 }
                 else {
                     jesno = false;
                 }
+                repaint();
             }
         }
     }
