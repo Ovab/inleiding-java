bf93d3787915e7d4e04c9c20fa60f652dc576ee0
Ovab <bavo.famknol@gmail.com>
2020-10-01 14:21:14 +0200

    cleaned up code, made H12.6 template. added text art in basic applet

---
+++ src/H12/H12_6.java
@@ -0,0 +1,98 @@
+package H12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class H12_6 extends Applet {
+    int[] searching;
+    int gezocht;
+    TextField txtfield;
+    Button okeknop;
+    int out;
+    int times;
+    boolean jesno;
+
+
+
+    public void init() {
+
+        searching = new int[5];
+        txtfield = new TextField("", 10);
+        add(txtfield);
+
+        okeknop = new Button("Confrim");
+        add(okeknop);
+        okeknop.addActionListener(new InputListener());
+    }
+
+
+    public void paint(Graphics g) {
+        if (jesno == true) {
+            g.drawString("Number found: index "+ out + "en komt " + times + " keer voor", 50 ,120);
+        }
+        if (jesno == false) {
+            g.drawString("ERROR: FUNNY NUMBER NOT FOUND", 50 ,120);
+        }
+    }
+
+    //classes
+    class InputListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+            searching [0] = 69;
+            searching [1] = 69420;
+            searching [2] = 420;
+            searching [3] = 420;
+            searching [4] = 420;
+
+            //getting txt
+            String b = txtfield.getText();
+            gezocht = Integer.parseInt(b);
+            //debug stuff
+            System.out.println("input is "+ gezocht);
+            for (int i = 0; i < searching.length; i++) {
+                //for debugging purposes, not needed anymore but it's cool for when I screw around with this program again
+                System.out.println("i is at "+out);
+                System.out.println(searching[i]);
+                //funny number check
+                if (gezocht == searching [i]) {
+                    jesno = true;
+                    out = i;
+                    repaint();
+                    //accidentally did 12.5 here XD anyway here the loop stops if the number has been found
+                    break;
+                }
+
+                else {
+                    jesno = false;
+                    repaint();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+/*/ Memes
+⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
+⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
+⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
+ */
