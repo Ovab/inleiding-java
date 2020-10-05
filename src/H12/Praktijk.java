e9b09ec075474ce170d0afdccb4fb0ae9549fb79
Ovab <bavo.famknol@gmail.com>
2020-10-05 09:52:51 +0200

    added some buttons
    Problems:  jsut doesnt work

--- src/H12/Praktijk.java
+++ src/H12/Praktijk.java
@@ -2,25 +2,40 @@ package H12;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Praktijk extends Applet {
-    TextField [] txtfieldarray;
+    TextField[] txtfieldarray;
+    Button confirm;
+    int i;
 
 
     public void init() {
-        txtfieldarray = new TextField[20];
-        for (int i = 0; i < txtfieldarray.length; i++) {
+        setSize(250, 300);
+        txtfieldarray = new TextField[18];
+        confirm = new Button("ok");
+        confirm.addActionListener(new InputListener());
+        add(confirm);
             txtfieldarray[i] = new TextField("", 10);
             add(txtfieldarray[i]);
-        }
+            repaint();
     }
 
 
+    public void paint(Graphics g) {
 
 
-    public void paint (Graphics g) {
+    }
 
-
-
+    class InputListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            i++;
+            System.out.println("button pressed");
+            if (i>=9) {
+                i = 20;
+                repaint();
+            }
+        }
     }
 }
