4e8ad8383ea80a0b991afac8e23939a2a5438a46
Ovab <bavo.famknol@gmail.com>
2020-10-08 10:06:28 +0200

    h13.1 and h13.2 done

--- src/h13/H13_1.java
+++ src/h13/H13_1.java
@@ -4,34 +4,32 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class H13_1 extends Applet {
-    int x1 = 10;
-    int x2 = 20;
-    int x3 = 30;
-    int y1 = 15;
-    int y2 = 25;
-    int y3 = 35;
-
+    int x1 = 100;
+    int x2 = 100;
+    int x3 = 200;
+    int y1 = 100;
+    int y2 = 200;
+    int y3 = 100;
 
 
     public void init() {
     }
 
 
-
-
     public void paint(Graphics g) {
-        g.drawPolygon(tekenDriehoek(50,10););
+        tekenDriehoek(g, x1, y1, x2, y2, x3, y3);
+        tekenDriehoek(g, 50, 50, 150, 250, 350, 250);
+        tekenDriehoek(g,100, 100, 150, 200, 50, 200);
     }
 
 
 
-    void tekenDriehoek( Graphics g,int x ) {
+    void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
         // teken een driehoek m.b.v. de meegegeven data (parameters)
-        int x[] = {100, 70, 130};
-        int y[] = {50, 100, 100};
-        g.drawPolygon(x,y , 3);
+        g.drawLine(x1, y1,x2,y2);
+        g.drawLine(x1, y1, x3, y3);
+        g.drawLine(x2, y2, x3, y3);
     }
-
 }
 
 
--- src/h13/H13_2.java
+++ src/h13/H13_2.java
@@ -4,22 +4,38 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class H13_2 extends Applet {
+    int x1 = 100;
+    int y1 = 100;
+    int x2 = 100;
+    int y2 = 50;
 
     public void init() {
-setBackground(Color.red);
 
 
     }
-
-
 
 
     public void paint(Graphics g) {
-        g.setColor(Color.black);
-            int x1 =+50;
-            g.fillRect(50, 15, 100,5);
+        muur(g, 50, 10,69, 69, 20, 10);
+        //tekensteen(g, x1, y1, x2, y2);
+    }
+
+    void tekensteen(Graphics g, int x1, int y1, int with, int height) {
+        // teken een driehoek m.b.v. de meegegeven data (parameters)
+        g.setColor(Color.red);
+        g.fillRect(x1, y1, with, height);
+        g.setColor(Color.GRAY);
+        g.drawRect(x1, y1, with, height);
+    }
+
+    void muur(Graphics g, int linkhoekX, int linkhoekY, int rij, int kolom, int breedsteen, int hoogsteen) {
+        for (int i = 0; i < rij; i++) {
+            for (int j = 0; j < kolom; j++) {
+                tekensteen(g, linkhoekX+i*breedsteen, linkhoekY+j*hoogsteen, breedsteen, hoogsteen);
+            }
         }
     }
+}
 
 
 
