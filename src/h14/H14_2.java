f18ba33b0371d215bce2636895a0cbdbbcafaf72
Ovab <bavo.famknol@gmail.com>
2020-10-18 23:54:07 +0200

    copied over card drawing from H14.1

--- src/h14/H14_2.java
+++ src/h14/H14_2.java
@@ -4,19 +4,35 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class H14_2 extends Applet {
-    int x[]={100,70,130};
-    int y[]={50,100,100};
-
+    int randomcolorint, randomtypeint, randomint;
+    String [] color;
+    String [] type;
     public void init() {
-
+        //declaring values of the array
+        color = new String[3];
+        type = new String[4];
+        color[0] = "Black";
+        color[1] = "Red";
+        type[0] = "Spade";
+        type[1] = "Diamond";
+        type[2] = "Hearts";
+        type[3] = "IDK the 4th one";
+        //making random value for the colors
+        randomcolorint = (int) (Math.random() * 2 + 0);
+        System.out.println(color[randomcolorint]);
+        //making random types
+        randomtypeint = (int) (Math.random() * 4 + 0);
+        System.out.println(type[randomtypeint]);
+        //making random types
+        randomint = (int) (Math.random() * 6 + 1);
+        System.out.println(randomint);
 
     }
 
 
 
-
     public void paint(Graphics g) {
-        g.drawPolygon(x,y , 3);
+        g.drawString(color[randomcolorint]+" "+type[randomtypeint]+ " "+randomint, 50, 50);
 
 
     }
