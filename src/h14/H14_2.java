a5dee5c68bb7a5261d2982b331e93162e8c839c0
Ovab <61050421+Ovab@users.noreply.github.com>
2020-10-19 12:01:06 +0200

    pc and laptop sync

--- src/h14/H14_2.java
+++ src/h14/H14_2.java
@@ -7,7 +7,9 @@ public class H14_2 extends Applet {
     int randomcolorint, randomtypeint, randomint;
     String [] color;
     String [] type;
+    String [] deck;
     public void init() {
+        deck = new String [52];
         //declaring values of the array
         color = new String[3];
         type = new String[4];
@@ -16,7 +18,7 @@ public class H14_2 extends Applet {
         type[0] = "Spade";
         type[1] = "Diamond";
         type[2] = "Hearts";
-        type[3] = "IDK the 4th one";
+        type[3] = "Clubs";
         //making random value for the colors
         randomcolorint = (int) (Math.random() * 2 + 0);
         System.out.println(color[randomcolorint]);
@@ -26,15 +28,36 @@ public class H14_2 extends Applet {
         //making random types
         randomint = (int) (Math.random() * 6 + 1);
         System.out.println(randomint);
-
+        System.out.println("");
+        System.out.println(deelKaart());
     }
 
 
 
     public void paint(Graphics g) {
         g.drawString(color[randomcolorint]+" "+type[randomtypeint]+ " "+randomint, 50, 50);
+    }
+    private String deelKaart() {
+        int random = (int) (Math.random() * deck.length);
+        System.out.println(random);
+        String kaart = deck[random];
 
-
+        //vervang de inhoud van deck met een nieuwe lijst
+        //het nieuwe deck is 1 kaart kleiner dan het oude deck
+        String[] hulpLijst = new String[deck.length - 1];
+        //om het nieuwe deck te vullen hebben we een index nodig
+        int hulpindex = 0;
+        //kopieer alle kaarten naar hulplijst behalve de kaart die net gedeeld is
+        for (int i = 0; i < deck.length; i++) {
+            if (i != random) {
+                hulpLijst[hulpindex] = deck[i];
+                hulpindex++;
+            }
+        }
+        //vervang nu deck met de nieuwe lijst
+        deck = hulpLijst;
+        //de methode geeft de random kaart terug
+        return kaart;
     }
 }
 
